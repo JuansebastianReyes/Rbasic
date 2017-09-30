72a20978d146c7232237586e7bb695f7ee359a1a
JuansebastianReyes <reyes_garcia1103@hotmail.com>
2017-09-30 02:06:05 -0500

    Se añade un ejemplo de funcion factorial

--- Ejemplos/funcionesSimples.R
+++ Ejemplos/funcionesSimples.R
@@ -1,8 +1,25 @@
 #declarar funcion suma 
 suma <- function(x,y){
-  s <- x + y
-  s
+  x + y
 }
-x <- 1
+
+#declara funcion potencia
+potencia <- function(x){
+  x*x
+}
+
+#declara funcion factorial
+factorial <- function(x){
+  if (x==0){
+    1
+  }else{
+    x * factorial(x-1)
+  }
+}
+
+x <- 5
 y <- 2
+
 suma(x,y)
+potencia(x)
+factorial(x)
